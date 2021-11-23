72d2765a6e193b61259253b83d238df6e35f6a09
Matija <matija_bozic@yahoo.com>
2021-11-23 20:21:20 +0000

    exported asserArrayEqual

---
+++ asserArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function (arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅ Array Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Array Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+module.exports = assertArraysEqual;
