8bf45032bc540ad67d1e939242181167dbe93be4
Matija <matija_bozic@yahoo.com>
2021-11-23 20:16:31 +0000

    Implemented eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,14 @@
+
+const eqArrays = function (arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = eqArrays;
