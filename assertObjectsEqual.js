6f3e54d77f3a9e9d8cb72f2d401af235dbce117d
Matija <matija_bozic@yahoo.com>
2021-11-25 18:39:10 +0000

    assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,29 +1,43 @@
-const eqObjects = function (object1, object2) {
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (let key of Object.keys(object1)) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      }
 
-    }
-    return true;
+  
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
   }
-  return false;
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 
-const assertObjectsEqual = function (actual, expected) {
-  const inspect = require(`util`).inspect;
-  if (actual !== expected) {
-
-    console.log(`Assertion Failed:  ${inspect(ab)} !== ${inspect(ba)}`);
-  } else {
-    console.log(`Assertion Passed:  ${inspect(ab)} === ${inspect(ba)}`);
+const eqObjects = function(object1, object2) {
+  let key1 = Object.keys(object1);
+  let key2 = Object.keys(object2);
+  if (key1.length !== key2.length) {
+    return false;
   }
-}
-module.exports = assertObjectsEqual;
-   const ab = { a: "4", b: "2"};
-   const ba = { b: "2", a: "1"};
-   console.log(assertObjectsEqual(eqObjects(ab,ba),false));
+  for (let item of key1) {
+    if (Array.isArray(object1[item])) {
+      if (!Array.isArray(object2[item])) {
+        return false;
+      } else if (!eqArrays(object1[item], object2[item])) {
+        return false;
+      }
+    } else if (object1[item] !== object2[item]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Array Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Array Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
