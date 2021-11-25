b51d876c21c3034ac02210eaf411a45c8c45a9ca
Matija <matija_bozic@yahoo.com>
2021-11-25 18:34:33 +0000

    assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,8 @@
-const assertEqual = function (actual, expected) {
+ const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed:  ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected}`);
   }
 };
+
