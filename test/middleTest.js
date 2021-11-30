17ac5450713f8268f64695913fc14cc28a89c296
Matija <matija_bozic@yahoo.com>
2021-11-30 04:10:46 +0000

    middleTest export

---
+++ test/middleTest.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+describe("#middle", () => {
+  it("returns empty array for arrays with 1 or 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), [])
+  });
+
+  it("returns single element for odd no of elements", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  });
+
+  it("returns array of 2 elements for odd even no of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  });
+});  
