15d5ea3d2d6c532694be4929fdefdcd5eeb0fc62
Matija <matija_bozic@yahoo.com>
2021-11-30 02:57:49 +0000

    tail export

--- tail.js
+++ tail.js
@@ -1,17 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`🛑 🛑 🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
+const tail = function(index) {
+  return index.slice(1);
 };
 
-const tail = function(array) {
-  return array.slice(1);
-};
+module.exports = tail;
 
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
