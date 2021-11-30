96b6eebfa4bfe50f2b257d1841e550f6d7c2b4bd
Matija <matija_bozic@yahoo.com>
2021-11-30 00:15:58 +0000

    export middle

--- middle.js
+++ middle.js
@@ -1,18 +1,20 @@
-const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
-
 const middle = function(array) {
-  let newArr = [];
-  if (array.length <= 2 && array.length > 0) {
-    //   console.log("The array has only 1 or 2 elements") 
-  } else if (array.length % 2 === 0) {
-    newArr = [array[(array.length / 2) - 1], array[array.length / 2]]
-    //   console.log("The array has an even number of elements"); 
-  } else if (array.length % 2 !== 0) {
-    newArr = [array[Math.floor(array.length / 2)]]
-    //   console.log("The array has an odd number of elements")
+  let middleArray = [];
+  if (array.length < 3) {
+    return middleArray;
+  } 
+  let middleOneIndex = Math.floor((array.length - 1 ) / 2);
+  var middleOne = array[middleOneIndex]
+  middleArray.push(middleOne);
+  if (array.length % 2 === 0) {
+    let middleTwoIndex = middleOneIndex + 1
+    let middleTwo = array[middleTwoIndex]
+    middleArray.push(middleTwo);
+
   }
-  return newArr;
-}
+  return middleArray;
+;}
+
 module.exports = middle;
