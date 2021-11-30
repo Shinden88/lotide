2e05296db12ad3444e71515f148c5cf2e98f1cc6
Matija <matija_bozic@yahoo.com>
2021-11-30 02:57:01 +0000

    head export

--- head.js
+++ head.js
@@ -1,17 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`🛑 🛑 🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
+const head = function (arr) {
+  return arr[0];
 };
 
-const head = function (array) {
-  return array[0];
-};
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
+ module.exports = head
